96c63a8cdb3b993751845feb0385188ce60c7bc8
Sneha Mavani <snehamavani.cloudus23@gmail.com>
2026-08-11 16:31:18 +0530

    Fix syntax error in AppServiceProvider logging

--- AppServiceProvider.php
+++ AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Log::info('AppServiceProvider registered');
+        Log::info('AppServiceProvider registered')
     }
 
     /**
